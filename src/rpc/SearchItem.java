3d36a9c2881c0751115e27306e9ec89d1ee297f5
ShawnXxy <shawnxxy@hotmail.com>
2017-08-02 23:49:38 +0800

    servelet and rpc setting up

--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -1,16 +1,21 @@
 package rpc;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Servlet implementation class SearchItem
  */
-@WebServlet("/SearchItem")
+@WebServlet("/search")
 public class SearchItem extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +32,33 @@ public class SearchItem extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		/*
+		 * TEST CONNECTION
+		 */
+		// Tells the browser that server is returning a response in a format of JSON
+		response.setContentType("application/json");
+		// Allow all viewers to view this response
+		response.addHeader("Access-Control-Allow-Origin", "*");
+		// Create a PrintWriter from response such  that we can add data to response
+		String username = "";
+//		PrintWriter  out = response.getWriter();
+		
+		if (request.getParameter("username") != null) {
+			username = request.getParameter("username"); // get the username sent from the client
+//			out.print("Hello " + username);
+		}
+		JSONObject obj = new JSONObject();
+		try {
+			obj.put("username", username);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		PrintWriter out = response.getWriter();
+		out.print(obj);
+		out.flush(); // Flush the output stream and send the data to the client side
+		out.close(); // Close this response
 	}
 
 	/**
