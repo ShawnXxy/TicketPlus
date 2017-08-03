2f9e90c82718b97bcc57afb06082a284b581aad3
ShawnXxy <shawnxxy@hotmail.com>
2017-08-03 08:59:28 +0800

    rpc

--- src/rpc/RpcHelper.java
+++ src/rpc/RpcHelper.java
@@ -1,5 +1,60 @@
 package rpc;
 
-public class RpcHelper {
+import java.io.BufferedReader;
+import java.io.PrintWriter;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+/*
+ * 	A helper class to handle rpc related parsing logics
+ */
+public class RpcHelper {
+	// Parse a JOSON object from http request
+	public static JSONObject readJsonObject (HttpServletRequest request) {
+		StringBuffer jb = new StringBuffer();
+		String line  = null;
+		try {
+			BufferedReader reader = request.getReader();
+			while ((line = reader.readLine()) != null) {
+				jb.append(line);
+			}
+			reader.close();
+			return new JSONObject(jb.toString());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	// Writes a JSONObject to http response
+	public static void writeJsonObject (HttpServletResponse response, JSONObject obj) {
+		try {
+			response.setContentType("application/json");
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			PrintWriter out = response.getWriter();
+			out.print(obj);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	// Writes a JSONArray to http response
+	public static void writeJsonArray(HttpServletResponse response, JSONArray array) {
+		try {
+			response.setContentType("application/json");
+			response.addHeader("Access-Control-Allow-Origin", "*");
+			PrintWriter out = response.getWriter();
+			out.println(array);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
--- src/rpc/SearchItem.java
+++ src/rpc/SearchItem.java
@@ -1,7 +1,6 @@
 package rpc;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -34,31 +34,39 @@ public class SearchItem extends HttpServlet {
 		// TODO Auto-generated method stub
 //		response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-		/*
-		 * TEST CONNECTION
-		 */
-		// Tells the browser that server is returning a response in a format of JSON
-		response.setContentType("application/json");
-		// Allow all viewers to view this response
-		response.addHeader("Access-Control-Allow-Origin", "*");
-		// Create a PrintWriter from response such  that we can add data to response
-		String username = "";
-//		PrintWriter  out = response.getWriter();
+//		/*
+//		 * TEST CONNECTION
+//		 */
+//		// Tells the browser that server is returning a response in a format of JSON
+//		response.setContentType("application/json");
+//		// Allow all viewers to view this response
+//		response.addHeader("Access-Control-Allow-Origin", "*");
+//		// Create a PrintWriter from response such  that we can add data to response
+//		String username = "";
+////		PrintWriter  out = response.getWriter();
+//		
+//		if (request.getParameter("username") != null) {
+//			username = request.getParameter("username"); // get the username sent from the client
+////			out.print("Hello " + username);
+//		}
+//		JSONObject obj = new JSONObject();
+//		try {
+//			obj.put("username", username);
+//		} catch (JSONException e) {
+//			e.printStackTrace();
+//		}
+//		PrintWriter out = response.getWriter();
+//		out.print(obj);
+//		out.flush(); // Flush the output stream and send the data to the client side
+//		out.close(); // Close this response
 		
-		if (request.getParameter("username") != null) {
-			username = request.getParameter("username"); // get the username sent from the client
-//			out.print("Hello " + username);
-		}
-		JSONObject obj = new JSONObject();
+		JSONArray array = new JSONArray();
 		try {
-			obj.put("username", username);
+			array.put(new JSONObject().put("username", "abcd"));
 		} catch (JSONException e) {
 			e.printStackTrace();
 		}
-		PrintWriter out = response.getWriter();
-		out.print(obj);
-		out.flush(); // Flush the output stream and send the data to the client side
-		out.close(); // Close this response
+		RpcHelper.writeJsonArray(response, array);
 	}
 
 	/**
