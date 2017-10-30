ccf0098aec5d74dd725d87b5313b6d805d8f99cb
ShawnXxy <shawnxxy@hotmail.com>
2017-10-30 15:06:29 -0500

    docker-test

---
+++ dbinit/init.sql
@@ -0,0 +1,6 @@
+USE ticket;
+CREATE TABLE items (item_id VARCHAR(255) NOT NULL, name VARCHAR(255), city VARCHAR(255), state VARCHAR(255), country VARCHAR(255), zipcode VARCHAR(255), rating FLOAT, address VARCHAR(255), latitude FLOAT, longitude FLOAT, description VARCHAR(255), snippet VARCHAR(255), snippet_url VARCHAR(255), image_url VARCHAR(255),url VARCHAR(255), PRIMARY KEY ( item_id ));
+CREATE TABLE categories (item_id VARCHAR(255) NOT NULL, category VARCHAR(255), PRIMARY KEY ( item_id, category), FOREIGN KEY (item_id) REFERENCES items(item_id));
+CREATE TABLE users (user_id VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, first_name VARCHAR(255), last_name VARCHAR(255), PRIMARY KEY ( user_id ));
+CREATE TABLE history (history_id bigint(20) unsigned NOT NULL AUTO_INCREMENT, user_id VARCHAR(255) NOT NULL , item_id VARCHAR(255) NOT NULL, last_favor_time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP, PRIMARY KEY (history_id), FOREIGN KEY (item_id) REFERENCES items(item_id), FOREIGN KEY (user_id) REFERENCES users(user_id));
+INSERT INTO users VALUES ("1111", "3229c1097c00d497a0fd282d586be050", "Xiangyu", "Xiao");
