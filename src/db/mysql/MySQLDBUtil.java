4d96e4a243d054f5858f8d3dd900c9875a317d95
ShawnXxy <shawnxxy@hotmail.com>
2019-03-31 18:57:39 +0800

    updates connection string

--- src/db/mysql/MySQLDBUtil.java
+++ src/db/mysql/MySQLDBUtil.java
@@ -1,10 +1,10 @@
 package db.mysql;
 
 public class MySQLDBUtil {
-    private static final String HOSTNAME = "localhost";
+    private static final String HOSTNAME = "shawnxxy.mysql.database.azure.com";
     private static final String PORT_NUM = "3306";
     public static final String DB_NAME = "ticket";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "root";
+    private static final String USERNAME = "x_xiao2_bnu@shawnxxy";
+    private static final String PASSWORD = "XXy@@4592995";
     public static final String URL = "jdbc:mysql://" + HOSTNAME + ":" + PORT_NUM + "/" + DB_NAME + "?user=" + USERNAME + "&password=" + PASSWORD + "&autoreconnect=true";
 }
